fa59bb46ce3c613dcbeb54ac305437947b08d352
Phil Plückthun <phil@plckthn.me>
2017-05-14 20:54:23 +0100

    Add loading spinner

--- src/client/loading.js
+++ src/client/loading.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import Redbox from 'redbox-react'
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const Spinner = styled.div`
+  display: block;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -15px;
+  margin-top: -15px;
+
+  animation: ${spin} 0.75s linear infinite;
+  width: 30px;
+  height: 30px;
+  border: 8px solid ${p => p.theme.colors.foreground};
+  border-right-color: transparent;
+  border-radius: 50%;
+`
 
 const Loading = ({ isLoading, error, pastDelay }) => {
   if (isLoading) {
-    return pastDelay ? <div>Loading...</div> : null // Don't flash "Loading..." when we don't need to.
-  } else {
-    return null
+    return pastDelay ? <Spinner /> : null
+  } else if (error) {
+    return <Redbox error={error} />
   }
+
+  return null
 }
 
 export default Loading

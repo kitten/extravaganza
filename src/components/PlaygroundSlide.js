3dfea08bad5ff64626a502bba7d8b4326bd26669
Phil Plückthun <phil@plckthn.me>
2017-05-16 14:06:01 +0100

    Update PlaygroundSlide theme

--- src/components/PlaygroundSlide.js
+++ src/components/PlaygroundSlide.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled, { css } from 'styled-components'
 
 import {
@@ -8,103 +8,129 @@ import {
   LivePreview
 } from 'react-live'
 
-const StyledProvider = styled(LiveProvider)`
-  display: flex;
-  flex-direction: column;
+const Wrapper = styled.div`
   height: 100%;
   width: 100%;
-  overflow: hidden;
+  padding: 2rem;
 `
 
-const LiveWrapper = styled.div`
+const Provider = styled(LiveProvider)`
   display: flex;
   flex-direction: row;
-  justify-content: stretch;
   align-items: stretch;
-  flex-grow: 1;
-`
 
-const column = css`
-  flex-basis: 50%;
+  height: 100%;
+  width: 100%;
+  border-radius: 6px;
+  overflow: hidden;
+
+  border: 1px solid #999;
+`;
+
+const Column = styled.div`
+  position: relative;
+  height: 100%;
   width: 50%;
-  max-width: 50%;
+  flex-basis: 50%;
+  flex-shrink: 0;
+
+  &:last-child {
+    border-left: 1px solid #333;
+  }
 `
 
-const StyledEditor = styled(LiveEditor)`
-  font-family: monospace;
-  overflow: scroll;
-  box-shadow: inset 6px 0 5px -5px rgba(0, 0, 0, 0.4);
+const Preview = styled(LivePreview)`
+  padding: 0.5rem;
+  height: 100%;
+  background: #fff;
+  color: #333;
+`;
 
-  display: block;
-  white-space: pre-wrap;
-  font-size: 70%;
-
-  background-color: #1D1F21;
-  color: #C5C8C6;
-
+const Editor = styled(LiveEditor)`
   padding: 0.5rem;
   margin: 0;
+  min-height: 100%;
+  font-size: 1.15vw;
 
+  white-space: pre-wrap;
   box-sizing: border-box;
   vertical-align: baseline;
   outline: none;
   text-shadow: none;
-  -webkit-hyphens: none;
-  -ms-hyphens: none;
   hyphens: none;
   word-wrap: normal;
   word-break: normal;
   text-align: left;
   word-spacing: normal;
-  -moz-tab-size: 2;
-  -o-tab-size: 2;
   tab-size: 2;
 
-  ${column}
+  color: rgba(233, 237, 237, 1);
+  background: #263238;
+
   ${p => p.theme.playgroundTheme}
-`
+`;
 
-const StyledPreview = styled(LivePreview)`
-  position: relative;
-  padding: 0.5rem;
-  background: ${p => p.background || p.theme.colors.background};
-  color: ${p => p.textColor || p.theme.colors.primary};
-  text-align: center;
-  overflow: hidden;
+const Title = styled.div`
+  width: 100%;
+  height: 1.7rem;
+  line-height: 1.7rem;
+  vertical-align: middle;
 
-  ${column}
-`
+  background: #272822;
+  border-bottom: 1px solid #333;
+  color: #fff;
 
-const StyledError = styled(LiveError)`
   display: block;
+  font-size: 1.15vw;
+  padding: 0 0.4rem;
+  text-transform: uppercase;
+
+  ${p => p.light && css`
+    background: #ddd;
+    border-bottom: 1px solid #999;
+    color: #424242;
+  `}
+`;
+
+const Error = styled(LiveError)`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: left;
   white-space: pre-wrap;
-  font-size: 30%;
-  padding: 16px;
-  background: #ff5555;
-  color: #f8f8f2;
-`
+  font-size: 1vw;
+
+  background: rgba(255, 35, 36, 0.8);
+  color: white;
+  padding: 0.4rem;
+`;
 
 const PlaygroundSlide = ({
   textColor,
   background,
   noInline,
-  code
+  code,
+  scope
 }) => (
-  <StyledProvider
-    code={code}
-    noInline={noInline}
-    mountStylesheet={false}
-  >
-    <LiveWrapper>
-      <StyledPreview
-        background={background}
-        textColor={textColor}
-      />
-      <StyledEditor />
-    </LiveWrapper>
+  <Wrapper>
+    <Provider
+      mountStylesheet={false}
+      code={code.trim()}
+      scope={{ Component, ...scope }}
+      noInline={noInline}
+    >
+      <Column>
+        <Title light>Live Preview</Title>
+        <Preview />
+        <Error />
+      </Column>
 
-    <StyledError />
-  </StyledProvider>
+      <Column>
+        <Title>Source Code</Title>
+        <Editor />
+      </Column>
+    </Provider>
+  </Wrapper>
 )
 
 export default PlaygroundSlide
